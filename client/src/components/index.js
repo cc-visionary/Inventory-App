64c050bdfaf1454364449a984971e32d426a5f15
Christopher Lim <lim.christopher.go@gmail.com>
2022-01-05 18:27:03 +0800

    Finished admin view for inventory

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import AddAccount from './AddAccount';
 import AdminEditAccount from './AdminEditAccount';
+import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
 
-export { Navbar, AddAccount, AdminEditAccount };
+export { Navbar, AddAccount, AdminEditAccount, AddProduct, EditProduct };
