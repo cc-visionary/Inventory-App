8468afe38db72baa6a449f6da2dec04a16fd63d5
Christopher Lim <lim.christopher.go@gmail.com>
2022-01-17 14:09:41 +0800

    Fixed add product placeholder, and fixed the deletion of admin in admin view of users page

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -52,7 +52,7 @@ const AddProduct = ({ errorMessage, visible, onOk, onCancel }) => {
             min={1}
             value={quantity} 
             onChange={(val) => setQuantity(val)} 
-            placeholder="Enter product name" 
+            placeholder="Enter quantity" 
             onKeyPress={(e) => e.key === 'Enter' && handleOk()}
             style={{width: '100%'}} 
           />
@@ -64,7 +64,7 @@ const AddProduct = ({ errorMessage, visible, onOk, onCancel }) => {
             min={1}
             value={price} 
             onChange={(val) => setPrice(val)} 
-            placeholder="Enter product name" 
+            placeholder="Enter price" 
             onKeyPress={(e) => e.key === 'Enter' && handleOk()}
             style={{width: '100%'}} 
           />
@@ -75,7 +75,7 @@ const AddProduct = ({ errorMessage, visible, onOk, onCancel }) => {
         name="supplier"
         value={supplier} 
         onChange={(e) => setSupplier(e.target.value)} 
-        placeholder="Enter product name" 
+        placeholder="Enter supplier" 
         onKeyPress={(e) => e.key === 'Enter' && handleOk()}
       />
       <label><strong>Stock Location</strong></label>
@@ -83,7 +83,7 @@ const AddProduct = ({ errorMessage, visible, onOk, onCancel }) => {
         name="location"
         value={location} 
         onChange={(e) => setLocation(e.target.value)} 
-        placeholder="Enter product name" 
+        placeholder="Enter location" 
         onKeyPress={(e) => e.key === 'Enter' && handleOk()}
       />
       <label><strong>Date Purchased</strong></label>
